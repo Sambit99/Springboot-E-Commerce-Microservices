d77e03135ec2e46b4e7884a48cbb1ba0a5f42de0
Sambit99 <sambitkumar75@gmail.com>
2025-05-27 18:52:32 +0530

    feat: Add SQL migration to insert initial inventory data

---
+++ inventory-service/src/main/resources/db/migration/V2__add_inventory.sql
@@ -0,0 +1,6 @@
+INSERT INTO t_inventory (quantity, sku_code)
+VALUES (100,"IPhone 16"),
+       (100,"IPhone 15"),
+       (100,"Samsung S25 Ultra"),
+       (100,"Oneplus 15"),
+       (100,"Google Pixel 9");
