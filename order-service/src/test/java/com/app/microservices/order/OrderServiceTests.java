bdb70c3bc8e883f12365462cdf213fee2e4c9b63
Sambit99 <sambitkumar75@gmail.com>
2025-05-27 17:55:28 +0530

    feat: Add integration tests for OrderService to verify order creation

---
+++ order-service/src/test/java/com/app/microservices/order/OrderServiceTests.java
@@ -0,0 +1,55 @@
+package com.app.microservices.order;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class OrderServiceTests {
+
+    @ServiceConnection
+    static MySQLContainer mySQLContainer =new MySQLContainer(DockerImageName.parse("mysql:8.3.0"));
+
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setup(){
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    static {
+        mySQLContainer.start();
+    }
+
+    @Test
+    void shouldCreateOrder(){
+        String requestBody = """
+                {
+                    "skuCode":"IPhone 16 Pro Max",
+                    "price":120000,
+                    "quantity":1
+                }
+                """;
+
+        var responseBodyString = RestAssured.given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("/api/v1/order")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .extract()
+                .body().toString();
+
+        assertEquals(responseBodyString,"Order placed successfully");
+    }
+}
