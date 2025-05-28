a4182962b722c1022fdd893896dbe67b6d5bcd04
Sambit99 <sambitkumar75@gmail.com>
2025-05-28 20:20:06 +0530

    feat: Refactor OrderService to use InventoryClient for stock verification

--- order-service/src/main/java/com/app/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/app/microservices/order/service/OrderService.java
@@ -1,10 +1,10 @@
 package com.app.microservices.order.service;
 
+import com.app.microservices.order.client.InventoryClient;
 import com.app.microservices.order.dto.OrderRequest;
 import com.app.microservices.order.model.Order;
 import com.app.microservices.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -13,10 +13,16 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class OrderService {
 
-    @Autowired
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public Order placeOrder(OrderRequest orderRequest){
+
+        boolean isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(),orderRequest.quantity());
+
+        if(!isProductInStock)
+            throw new RuntimeException("Product with SkuCode "+orderRequest.skuCode()+" is out of stock");
+
         // Map OrderRequest to Order
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
