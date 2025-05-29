65df517b1755a0e50beacc7e1e3487968b1ec9f6
Sambit99 <sambitkumar75@gmail.com>
2025-05-29 20:00:27 +0530

    test: Fix order creation test with correct SKU and quantity, and update API endpoint

--- order-service/src/test/java/com/app/microservices/order/OrderServiceTests.java
+++ order-service/src/test/java/com/app/microservices/order/OrderServiceTests.java
@@ -1,20 +1,23 @@
 package com.app.microservices.order;
 
+import com.app.microservices.order.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 public class OrderServiceTests {
 
     @ServiceConnection
-    static MySQLContainer mySQLContainer =new MySQLContainer(DockerImageName.parse("mysql:8.3.0"));
+    static MySQLContainer mySQLContainer = new MySQLContainer(DockerImageName.parse("mysql:8.3.0"));
 
     @LocalServerPort
     private Integer port;
@@ -33,17 +36,19 @@ public class OrderServiceTests {
     void shouldCreateOrder(){
         String requestBody = """
                 {
-                    "skuCode":"IPhone 16",
+                    "skuCode":"IPhone_16",
                     "price":120000,
-                    "quantity":10
+                    "quantity":1
                 }
                 """;
+
+        InventoryClientStub.stubInventoryCall("IPhone_16",1);
 
         var responseBodyString = RestAssured.given()
                 .contentType("application/json")
                 .body(requestBody)
                 .when()
-                .post("api/v1/order")
+                .post("/api/v1/order")
                 .then()
                 .log().all()
                 .statusCode(201)
