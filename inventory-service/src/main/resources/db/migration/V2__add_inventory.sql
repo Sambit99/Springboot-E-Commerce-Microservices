bec29338224024e6e16cd65828ef28408c2aa90e
Sambit99 <sambitkumar75@gmail.com>
2025-05-29 20:00:36 +0530

    fix: Correct SKU formatting in inventory SQL migration

--- inventory-service/src/main/resources/db/migration/V2__add_inventory.sql
+++ inventory-service/src/main/resources/db/migration/V2__add_inventory.sql
@@ -1,6 +1,6 @@
 INSERT INTO t_inventory (quantity, sku_code)
-VALUES (100,"IPhone 16"),
-       (100,"IPhone 15"),
-       (100,"Samsung S25 Ultra"),
-       (100,"Oneplus 15"),
-       (100,"Google Pixel 9");
+VALUES (100,"IPhone_16"),
+       (100,"IPhone_15"),
+       (100,"Samsung_S25_Ultra"),
+       (100,"Oneplus_15"),
+       (100,"Google_Pixel_9");
