d6bd75bc3fce7caae28ba4db1b34cac18252de06
Sambit99 <sambitkumar75@gmail.com>
2025-05-29 20:00:19 +0530

    feat: Add InventoryClientStub for mocking inventory service calls in tests

---
+++ order-service/src/test/java/com/app/microservices/order/stubs/InventoryClientStub.java
@@ -0,0 +1,13 @@
+package com.app.microservices.order.stubs;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+public class InventoryClientStub {
+    public static void stubInventoryCall(String skuCode, Integer quantity){
+        stubFor(get(urlEqualTo("/api/v1/inventory?skuCode="+skuCode+"&quantity="+quantity))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type","application/json")
+                        .withBody("true")));
+    }
+}
