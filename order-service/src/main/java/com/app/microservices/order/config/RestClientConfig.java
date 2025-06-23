e0b753c91978a72bf16ed0a8a16a769f8cc04872
Sambit99 <sambitkumar75@gmail.com>
2025-06-23 20:23:27 +0530

    feat: Add RestClientConfig for InventoryClient integration

---
+++ order-service/src/main/java/com/app/microservices/order/config/RestClientConfig.java
@@ -0,0 +1,28 @@
+package com.app.microservices.order.config;
+
+import com.app.microservices.order.client.InventoryClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+public class RestClientConfig {
+
+    @Value("${inventory.url}")
+    private String inventoryUrl;
+
+    @Bean
+    public InventoryClient inventoryClient(){
+        RestClient restClient = RestClient.builder()
+                .baseUrl(inventoryUrl)
+                .build();
+
+        RestClientAdapter restClientAdapter = RestClientAdapter.create(restClient);
+        HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
+        return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+
+}
