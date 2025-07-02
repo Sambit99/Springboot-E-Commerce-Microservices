828db227f585a7132fb891b2bc4e999821a692e5
Sambit99 <sambitkumar75@gmail.com>
2025-07-02 21:01:06 +0530

    feat: Integrate Kafka messaging in OrderService for order placement events

--- order-service/src/main/java/com/app/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/app/microservices/order/service/OrderService.java
@@ -2,19 +2,25 @@ package com.app.microservices.order.service;
 
 import com.app.microservices.order.client.InventoryClient;
 import com.app.microservices.order.dto.OrderRequest;
+import com.app.microservices.order.event.OrderPlacedEvent;
 import com.app.microservices.order.model.Order;
 import com.app.microservices.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.KafkaClient;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public Order placeOrder(OrderRequest orderRequest){
 
@@ -33,6 +39,13 @@ public class OrderService {
         // Save Order to OrderRepository
         Order savedOrder = orderRepository.save(order);
 
+        // Send message to Kafka Topic
+        // OrderNumber, E-Mail
+
+        OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(),"abc@example.com");
+        log.info("Start - Sending OrderPlaceEvent to Kafka topic order-placed",orderPlacedEvent);
+        kafkaTemplate.send("order_placed",orderPlacedEvent);
+        log.info("End - Sending OrderPlaceEvent to Kafka topic order-placed",orderPlacedEvent);
         return savedOrder;
     }
 }
